8d653922820777ca4c0434d477d4d74ccd73be1a
Noobwastaken99 <czmoni99@yahoo.com>
2023-03-15 13:31:34 -0500

    got bonus finished

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -10,21 +10,39 @@ export class OrbitCountsComponent implements OnInit {
 
 	@Input() satellites: Satellite[];
 
+	countItems: string[] = ['Total:', 'Communication:', 'Probe:', 'Space Station:', 'Telescope:', 'Positioning:', 'Space Debris:'];
+
   constructor() { }
 
   ngOnInit() {
   }
 
-  countByType(type: string): number {
-	let count = 0;
-	if (this.satellites) {
-	  for (let i = 0; i < this.satellites.length; i++) {
-		 if (this.satellites[i].type === type) {
-			count++;
-		 }
-	  }
-	}
-	return count;
+//   countByType(type: string): number {
+// 	let count = 0;
+// 	if (this.satellites) {
+// 	  for (let i = 0; i < this.satellites.length; i++) {
+// 		 if (this.satellites[i].type === type) {
+// 			count++;
+// 		 }
+// 	  }
+// 	}
+// 	return count;
+//  }
+
+	countByType(type: string): number {
+    let count = 0;
+    console.log(type,type.substring(0,type.length-1))
+    type = type.substring(0,type.length-1);
+    if (this.satellites && type !== 'Total') {
+      for (let i = 0; i < this.satellites.length; i++) {
+         if (this.satellites[i].type === type) {
+            count++;
+         }
+      }
+      return count;
+    } else {
+        return this.satellites.length;
+    }
  }
 
 }
